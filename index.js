8673cdcab6e57c103bddd770e46ffa80d3c30af9
Tao Bian <biantao0519@hotmail.com>
2022-07-27 14:24:53 +1000

    working on activite funs

--- index.js
+++ index.js
@@ -5,24 +5,27 @@ const cTable = require('console.table')
 
 // connect to database
 connection.connect((err) => {
-    // if err
+    // if err, print err
     if (err) return console.log(err.message)
-    // if no err console WELCOME and run app
+    // if no err, print WELCOME and run app
     console.log('WELCOME TO EMPLOYEE TRACKER\n')
     startApp()
 })
 
+// prepare inquirer questions
+const startQuestions = [{
+    type: 'list',
+    name: 'startQuestions',
+    message: 'Please select what would you like to do?',
+    choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role', 'Exit Application']
+}]
+
 // start App
 const startApp = () => {
     // ask questions
-    inquirer.prompt({
-        type: 'list',
-        name: 'startQuestions',
-        message: 'Please select what would you like to do?',
-        choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role', 'Exit Application']
-    })
+    inquirer.prompt(startQuestions)
         // run functions based on user input
-        .then((answer) => {
+        .then(answer => {
             switch (answer.startQuestions) {
                 // if view all depart, run viewAllDepart function
                 case 'View All Departments':
@@ -38,15 +41,15 @@ const startApp = () => {
                     break
                 // if add depart, run add depart fn
                 case 'Add a Department':
-                    addDepartment()
+                    createDepartment()
                     break
                 // if add role, run add role
                 case 'Add a Role':
-                    addRole()
+                    createRole()
                     break
                 // if add employee, run add employee fn
                 case 'Add an Employee':
-                    addEmployee()
+                    createEmployee()
                     break
                 // if update employee role, fun update fn
                 case 'Update an Employee Role':
@@ -60,19 +63,78 @@ const startApp = () => {
         })
 }
 
-// view department fnc
+// view all department fn
 const viewAllDepartments = () => {
+    // SELECT from department
     const query = 'SELECT * FROM department'
     connection.query(query, (err, res) => {
+        // if error, print err
         if (err) return console.log(err.message)
-        
+        // if not, print department table
+        console.log('View All Departments:\n')
+        console.table(res)
+        // call start fn after print
+        startApp()
     })
 }
 
+// view all roles fn
+const viewAllRoles = () => {
+    // SELECT from role
+    const query = 'SELECT * FROM role'
+    connection.query(query, (err, res) => {
+        // if err, print err
+        if (err) return console.log(err.message)
+        // if not, print role table
+        console.log('View All Roles:\n')
+        console.table(res)
+        // call start app after print
+        startApp()
+    })
+}
 
-viewAllRoles()
-viewAllEmployees()
-addDepartment()
-addRole()
-addEmployee()
+// view all employees fn
+const viewAllEmployees = () => {
+    // SELECT from employee
+    const query = 'SELECT * FROM employee'
+    connection.query(query, (err, res) => {
+        // if err, print err
+        if (err) return console.log(err.message)
+        // if not, print role table
+        console.log('View All Employees:\n')
+        console.table(res)
+        // call start app after print
+        startApp()
+    })
+}
+
+// create new depart fn 
+const createDepartment = () => {
+    // ask question, input new dpt name
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'newDepartmentName',
+            message: 'Please enter the new department name'
+        }
+    ])
+    .then(answer => {
+        const newDepartmentName = answer.newDepartmentName
+        // INSERT new dpt into department table
+        connection.query('INSERT INTO department (name) VALUES (?)', [newDepartmentName], (err, res) => {
+            // if err, print err
+            if (err) return console.log(err.message)
+        })
+        // if no err, print dpt table and back to start
+        viewAllDepartments()
+    })
+}
+
+// create new role fn
+const createRole = () => {
+    // ask new role name, salary, department
+    
+}
+
+createEmployee()
 updateEmployeeRole()
