a14ed6be37fc449aaaa722a5cd5215fbc21df948
Tao Bian <biantao0519@hotmail.com>
2022-07-31 12:17:45 +1000

    undate view by dpt and manager

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const startApp = () => {
         type: 'list',
         name: 'startQuestions',
         message: 'Please select what would you like to do?',
-        choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role', 'Exit Application']
+        choices: ['View All Departments', 'View All Roles', 'View All Employees', 'View Employees by Department','View Employees by Manager', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role', 'Exit Application']
     }]
     // ask questions
     inquirer.prompt(startQuestions)
@@ -38,6 +38,14 @@ const startApp = () => {
                 // if view all employees, run view employees function    
                 case 'View All Employees':
                     viewAllEmployees()
+                    break
+                // -- if view employees by department, run view employee by manager func
+                case 'View Employees by Department':
+                    viewEmployeesByDepartment()
+                    break    
+                // -- if view employees by manager, run view employee by manager func
+                case 'View Employees by Manager':
+                    viewEmployeesByManager()
                     break
                 // if add depart, run add depart fn
                 case 'Add a Department':
@@ -72,7 +80,7 @@ const viewAllDepartments = () => {
         // if error, print err
         if (err) return console.log(err.message)
         // if not, print department table
-        console.log('View All Departments:\n')
+        console.log('\nView All Departments:')
         console.table(res)
         // call start fn after print
         startApp()
@@ -82,8 +90,7 @@ const viewAllDepartments = () => {
 
 // view all roles fn
 const viewAllRoles = () => {
-    // SELECT from role
-    // const query = `SELECT * FROM role`
+    // SELECT join role and department table by id
     const query = `SELECT role.id, role.title, department.name AS department, CONCAT('$', role.salary) AS salary 
     FROM role
     LEFT JOIN department ON role.department_id = department.id
@@ -92,7 +99,7 @@ const viewAllRoles = () => {
         // if err, print err
         if (err) return console.log(err.message)
         // if not, print role table
-        console.log('View All Roles:\n')
+        console.log('\nView All Roles:')
         console.table(res)
         // call start app after print
         startApp()
@@ -102,8 +109,7 @@ const viewAllRoles = () => {
 
 // view all employees fn
 const viewAllEmployees = () => {
-    // SELECT from employee
-    // const query1 = `SELECT * FROM employee`
+    // SELECT join department, role and employee table by id
     const query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, CONCAT('$', role.salary) AS salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager 
     FROM employee 
     LEFT JOIN role ON employee.role_id = role.id 
@@ -115,35 +121,88 @@ const viewAllEmployees = () => {
         // if err, print err
         if (err) return console.log(err.message)
         // if not, print role table
-        console.log('View All Employees:\n')
+        console.log('\nView All Employees:')
         console.table(res)
         // call start app after print
         startApp()
     })
 }
 
-/**
- * sql
- * select ...
- * from ... 
- *   join
- * where
- * group by 
- * order by
- * 
- */
 
-// create new depart fn 
-const createNewDepartment = () => {
-    // ask question, input new dpt name
+// -------------view by department------------------
+const viewEmployeesByDepartment = () => {
     inquirer.prompt([
         {
             type: 'input',
-            name: 'newDepartmentName',
-            message: 'Please enter the new department name'
+            name: 'departmentID',
+            message: 'Please enter departmentID to view employees'
         }
     ])
-        .then(answer => {
+    .then(answer => {
+        // SELECT join employee table, role table and department table by id, and choose row when departmentID =
+        const query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, CONCAT('$', role.salary) AS salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager 
+        FROM employee 
+            LEFT JOIN role ON employee.role_id = role.id 
+            LEFT JOIN department ON role.department_id = department.id 
+            LEFT JOIN employee manager ON manager.id = employee.manager_id
+            WHERE department.id = ?`
+            connection.query(query, [answer.departmentID], (err, res) => {
+                // if err, print err
+                if (err) return console.log(err.message)
+                // if not, print table
+                console.log('\nView Employees By Department:')
+                console.table(res)
+                startApp()
+            })
+    })
+}
+
+
+
+// ----------- view employees by manager-------------
+const viewEmployeesByManager = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'managerID',
+            message: 'Please enter managerID to view employees'
+        }
+    ])
+    .then(answer => {
+        // SELECT join employee table, role table and department table by id, and choose row when managerID =
+        const query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, CONCAT('$', role.salary) AS salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager 
+        FROM employee 
+            LEFT JOIN role ON employee.role_id = role.id 
+            LEFT JOIN department ON role.department_id = department.id 
+            LEFT JOIN employee manager ON manager.id = employee.manager_id
+            WHERE manager.id = ?`
+            
+            connection.query(query, [answer.managerID], (err, res) => {
+                // if err, print err
+                if (err) return console.log(err.message)
+                // if not, print table
+                console.log('\nView Employees By Manager:')
+                console.table(res)
+                startApp()
+            })
+        })
+    }
+    
+    
+    
+    
+    
+    // create new depart fn 
+    const createNewDepartment = () => {
+        // ask question, input new dpt name
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'newDepartmentName',
+                message: 'Please enter the new department name'
+            }
+    ])
+    .then(answer => {
             // INSERT new dpt into department table
             const newDepartmentName = answer.newDepartmentName
             const query = 'INSERT INTO department (name) VALUES (?)'
@@ -156,16 +215,16 @@ const createNewDepartment = () => {
             // call viewAllDepartments(), print dpt table and back to start
             viewAllDepartments()
         })
-}
-
-
-
-// -------------------------------------
-// SELECT fn for creating department array
-const createDepartmentsArray = () => {
-    let departmentsArray = []
-    const query = 'SELECT name FROM department'
-    connection.query(query, (err, res) => {
+    }
+    
+    
+    
+    // -------------------------------------
+    // SELECT fn for creating department array
+    const createDepartmentsArray = () => {
+        let departmentsArray = []
+        const query = 'SELECT name FROM department'
+        connection.query(query, (err, res) => {
         // if err, print err
         if (err) return console.log(err.message)
         // if not, create department array
@@ -199,66 +258,66 @@ const createNewRole = () => {
     ]
 
     inquirer.prompt(newRoleQuestions)
-        .then(answer => {
-            // SELECT get id by department name
-            let departmentID = 0
-            const newRoleDepartment = answer.newRoleDepartment
-            const query = 'SELECT id FROM department WHERE name = ?'
-            connection.query(query, newRoleDepartment, (err, res) => {
+    .then(answer => {
+        // SELECT get id by department name
+        let departmentID = 0
+        const newRoleDepartment = answer.newRoleDepartment
+        const query = 'SELECT id FROM department WHERE name = ?'
+        connection.query(query, newRoleDepartment, (err, res) => {
+            // if err, print err
+            if (err) return console.log(err.message)
+            // if not, get id
+            res.forEach(id => departmentID = id.id)
+            
+            // INSERT role by depart id
+            const newRole = {
+                title: answer.newRoleTitle,
+                salary: answer.newRoleSalary,
+                department_id: departmentID
+            }
+            const query = 'INSERT INTO role SET ?'
+            connection.query(query, newRole, (err, res) => {
                 // if err, print err
                 if (err) return console.log(err.message)
-                // if not, get id
-                res.forEach(id => departmentID = id.id)
-
-                // INSERT role by depart id
-                const newRole = {
-                    title: answer.newRoleTitle,
-                    salary: answer.newRoleSalary,
-                    department_id: departmentID
-                }
-                const query = 'INSERT INTO role SET ?'
-                connection.query(query, newRole, (err, res) => {
-                    // if err, print err
-                    if (err) return console.log(err.message)
-                    // if no err, print message
-                    return console.log(`\nNew Role ${answer.newRoleTitle} has been added successfully!\n`)
-                })
+                // if no err, print message
+                return console.log(`\nNew Role ${answer.newRoleTitle} has been added successfully!\n`)
+            })
                 // call viewAllRoles(), print roles table and back to start
                 viewAllRoles()
             })
         })
-}
-// ------------------------------------------
-
-
-// ---------------------------------------
-// SELECT create role array
-const createRoleArray = () => {
-    let rolesArray = []
-    const query = 'SELECT title FROM role'
-    connection.query(query, (err, res) => {
-        // if err, print err
-        if (err) return console.log(err.message)
-        // if not, create department array
-        res.forEach(data => rolesArray.push(data.title))
-    })
-    // return role array
-    return rolesArray
-}
-
-// SELECT create employee array for choose manager
-const createEmployeeArray = () => {
-    let firstNameArray = []
-    let lastNameArray = []
+    }
+    // ------------------------------------------
+    
+    
+    // ---------------------------------------
+    // SELECT create role array
+    const createRoleArray = () => {
+        let rolesArray = []
+        const query = 'SELECT title FROM role'
+        connection.query(query, (err, res) => {
+            // if err, print err
+            if (err) return console.log(err.message)
+            // if not, create department array
+            res.forEach(data => rolesArray.push(data.title))
+        })
+        // return role array
+        return rolesArray
+    }
+    
+    // SELECT create employee array for choose manager
+    const createEmployeeArray = () => {
+        let firstNameArray = []
+        let lastNameArray = []
     let employeesArray = []
-
+    
     const firstNameQuery = 'SELECT first_name FROM employee'
     connection.query(firstNameQuery, (err, res) => {
         // if err, print err
         if (err) return console.log(err.message)
         // if not, create first name array
         res.forEach(data => firstNameArray.push(data.first_name))
-
+        
         const lastNameQuery = 'SELECT last_name FROM employee'
         connection.query(lastNameQuery, (err, res) => {
             // if err, print err
@@ -266,7 +325,7 @@ const createEmployeeArray = () => {
             // if not, create last name array
             res.forEach(data => lastNameArray.push(data.last_name))
         })
-
+        
         // loop two arrays and join to get employee array
         for (let i = 0; i < firstNameArray.length; i++) {
             let employee = `${firstNameArray[i]} ${lastNameArray[i]}`
@@ -306,27 +365,27 @@ const createNewEmployee = () => {
             choices: createEmployeeArray()
         }
     ]
-
+    
     inquirer.prompt(newEmployeeQuestions)
-        .then(answer => {
-            let roleID = 0
-            let managerID = 0
-
-            //get roleID by name
+    .then(answer => {
+        let roleID = 0
+        let managerID = 0
+        
+        //get roleID by name
             const query = 'SELECT id FROM role WHERE title = ?'
             connection.query(query, answer.role, (err, res) => {
                 // if err, print err
                 if (err) return console.log(err.message)
                 // if not, get role id
                 res.forEach(id => roleID = id.id)
-
+                
                 let managerName = ''
                 // get manager name from answer
                 for (let i = 0; i < answer.manager.length; i++) {
                     if (answer.manager === '') return console.log('No manager found!')
                     managerName += answer.manager.charAt(i)
                 }
-
+                
                 // SELECT get manager id from manager name
                 const query = 'SELECT id FROM employee WHERE first_name = ?'
                 connection.query(query, managerName, (err, res) => {
@@ -334,8 +393,8 @@ const createNewEmployee = () => {
                     if (err) return console.log(err.message)
                     // if not, get manager id
                     res.forEach(id => managerID = id.id)
-
-
+                    
+                    
                     // INSERT new employee into table
                     const newEmployee = {
                         first_name: answer.firstName,
@@ -343,39 +402,39 @@ const createNewEmployee = () => {
                         role_id: roleID,
                         manager_id: managerID
                     }
-
+                    
                     const insertQuery = 'INSERT INTO employee SET ?'
-
+                    
                     connection.query(insertQuery, newEmployee, (err, res) => {
                         // if err, print err
                         if (err) return console.log(err.message)
                         // if no err, print message
                         return console.log(`\nNew Employee ${answer.firstName} ${answer.lastName} has been added successfully!\n`)
                     })
-
+                    
                     viewAllEmployees()
                 })
-
+                
             })
         })
-}
-//---------------------
-
-
-
-
-
-//---------------------------
-// fun for update employee role
-const updateEmployeeRole = () => {
-    // prepare questions
-    const questions = [
-        {
-            type: 'number',
-            name: 'employeeID',
-            message: 'Please enter employee ID to update'
-        },
-        {
+    }
+    //---------------------
+    
+    
+    
+    
+    
+    //---------------------------
+    // fun for update employee role
+    const updateEmployeeRole = () => {
+        // prepare questions
+        const questions = [
+            {
+                type: 'number',
+                name: 'employeeID',
+                message: 'Please enter employee ID to update'
+            },
+            {
             type: 'number',
             name: 'roleID',
             message: 'Please enter the role ID you wish to update to'
@@ -383,19 +442,50 @@ const updateEmployeeRole = () => {
     ]
     // ask questions
     inquirer.prompt(questions)
-        .then(answer => {
-            // UPDATE employee's role by id
-            const query = 'UPDATE employee SET role_id = ? WHERE id = ?'
-            connection.query(query, [answer.roleID, answer.employeeID], (err, res) => {
-                // if err, print err
-                if (err) return console.log(err.message)
-                // if no err, print message
-                console.log(`\nEmployee's role has been updated successfully!\n`)
-                viewAllEmployees()
-            })
+    .then(answer => {
+        // UPDATE employee's role by id
+        const query = 'UPDATE employee SET role_id = ? WHERE id = ?'
+        connection.query(query, [answer.roleID, answer.employeeID], (err, res) => {
+            // if err, print err
+            if (err) return console.log(err.message)
+            // if no err, print message
+            console.log(`\nEmployee's role has been updated successfully!\n`)
+            viewAllEmployees()
         })
-
+    })
+    
 }
 
 
 
+// //-----------Update employee manager-----------
+// const updateEmployeeManager = () => {
+
+//     inquirer.prompt([
+//         {
+//             type: 'input',
+//             name: 'employeeID',
+//             message: 'Please enter employeeID to update'
+//         },
+//         {
+//             type: 'input',
+//             name: 'managerID',
+//             message: 'Please enter the role ID you wish to update to'
+//         }
+//     ])
+//         .then(answer => {
+//             const query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, CONCAT('$', role.salary) AS salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager 
+//             FROM employee 
+//             LEFT JOIN role ON employee.role_id = role.id 
+//             LEFT JOIN department ON role.department_id = department.id 
+//             LEFT JOIN employee manager ON manager.id = employee.manager_id
+//             WHERE employee.id = ?`
+
+//             connection.query(query, [answer.employeeID], (err, res) => {
+//                 // if err, print err
+//                 if (err) return console.log(err.message)
+//                 // if not
+                
+//             })
+//         })
+// }
